a6e17194587a502f0dd5a25dd73e7f8800260f41
kenzitt27 <141749592+kenzitt27@users.noreply.github.com>
2023-11-03 10:42:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit
-import pandas
-import requests
-#import snowflake.connector
-import urllib.error 
-#import URLError
+import urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -15,7 +11,7 @@ streamlit.text('🥑🍞 Avocade Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -29,17 +25,20 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.write('The user entered ', fruit_choice)
-   else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-#except URLError as e:
-   # streamlit.error()
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-import snowflake.connector
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
+
+
+
+
 
